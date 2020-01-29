cea732c2f0db2eccd769b9f95a59e66f03eed9d6
Grawa <014501450145Aa>
2020-01-29 19:07:26 +0100

    fix translations

--- OAPro_assistant_button_script.py
+++ OAPro_assistant_button_script.py
@@ -2,10 +2,10 @@ from gpiozero import Button
 from time import sleep
 import keyboard
 
-pulsante = Button(26,pull_up=True)
+button = Button(26, pull_up=True)
 
 while True:
-    if pulsante.is_pressed:
+    if button.is_pressed:
         keyboard.press("m")
         print("google assistant triggered!")
         sleep(0.30)
